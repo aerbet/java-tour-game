78a992c24b982475ecf0488c24cb6649106bd586
Erbol Zhaparov <ae.270718@gmail.com>
2025-08-13 21:52:11 +0600

    styling output

--- test.java
+++ test.java
@@ -1,20 +1,20 @@
 public class test {
     public static void main(String[] args) {
         int[] rounds = {1, 2, 3};
-        int[] scores = {3, 6, 5};
+        int[] scores = {13, 16, 15};
         int[] attempts = {3, 2, 1};
         int totalScore = 14;
         int totalAttempts = 6;
 
         System.out.printf(
             "----------------- %s ----------------%n" +
-            "|   %s   | %18s | %-8s |%n" +
+            "|   %s   |       %s        | %-8s |%n" +
             "|-----------|--------------------|----------|%n" +
-            "|   - %d -   | %-18d | %-8d |%n" +
-            "|   - %d -   | %-18d | %-8d |%n" +
-            "|   - %d -   | %-18d | %-8d |%n" +
-            "%n---%n%n" +
-            "%-10s %-19d %-8d%n",
+            "|   - %d -   | %9d          |     %-4d |%n" +
+            "|   - %d -   | %9d          |     %-4d |%n" +
+            "|   - %d -   | %9d          |     %-4d |%n" +
+            "|-----------|--------------------|----------|%n" +
+            "    %-17s %-15d %-8d%n",
              "Finish game", "Round", "Score", "Attempts",
             rounds[0], scores[0], attempts[0],        
             rounds[1], scores[1], attempts[1],        
